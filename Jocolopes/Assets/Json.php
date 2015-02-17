cf367e433dca7a033823859195c3eacd8bb1beb5
DPr00f <jocolopes@gmail.com>
2015-02-17 12:54:25 +0000

    Finished loading assets

--- Jocolopes/Assets/Json.php
+++ Jocolopes/Assets/Json.php
@@ -6,13 +6,89 @@ use Jocolopes\Filefilter;
 
 class Json {
 
-    protected $loadedFiles;
+    protected $assets;
     protected $assetsRootFolder;
     protected $types;
+    protected $jsonFile;
+    protected $assetsJson;
+    protected $fFilter;
 
-    function __construct($jsonFile, $assetsRootFolder, $typesClass = '\Jocolopes\Request\Types') {
-        $this->loadedFiles      = array('head' => [], 'body' => []);
+
+    public function __construct($jsonFile, $assetsRootFolder, $typesClass = '\Jocolopes\Request\Types') {
+        $this->jsonFile         = $jsonFile;
         $this->assetsRootFolder = $assetsRootFolder;
         $this->types            = new $typesClass();
+        $this->types            = $this->types->getTypes();
+        $this->fFilter          = new Filefilter();
+    }
+
+    public function setJsonFile($jsonFile){
+        $this->jsonFile         = $jsonFile;
+    }
+
+    public function loadJsonFile(){
+        return json_decode( file_get_contents($this->jsonFile) ); 
+    }
+
+    public function getAssets(){
+        $this->assets = array('head' => [], 'body' => []);
+        $this->assetsJson = $this->loadJsonFile();
+        foreach($this->types as $type){
+            $this->loadFiles($type, 'head');
+            $this->loadFiles($type, 'body');
+        }
+
+        return $this->assets;
+    }
+
+    public function loadFiles($type, $position)
+    {
+        if(property_exists($this->assetsJson, $type) and property_exists($this->assetsJson->$type, $position)){
+            $filesArray = $this->assetsJson->$type->$position;
+            foreach($filesArray as $file){
+                $fileWithRoot = $this->assetsRootFolder . $file;
+                if(is_file($fileWithRoot)){
+                    if(!in_array($file, $this->assets[$position])){
+                        array_push($this->assets[$position], $file);
+                    }
+                }else{
+                    if(strpos($fileWithRoot, '*') === FALSE){
+                        throw new \Exception("File '$fileWithRoot' couldn't be found");
+                    }
+                    $files = $this->fFilter->scan($fileWithRoot);
+                    foreach($files as $scannedFile){
+                        $scannedFile = substr($scannedFile, strlen($this->assetsRootFolder));
+                        if(!in_array($scannedFile, $this->assets[$position])){
+                            array_push($this->assets[$position], $scannedFile);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    public function getAssetsWithTag(){
+        $assets = $this->getAssets();
+        $head = $assets['head'];
+        $body = $assets['body'];
+        $newHead = array();
+        $newBody = array();
+        foreach($head as $file){
+            if(strpos( $file, '.js' ) !== FALSE){
+                array_push($newHead, '<script type="text/javascript" src="'. $file .'"></script>');
+            }else if(strpos( $file, '.css' ) !== FALSE){
+                array_push($newHead, '<link rel="stylesheet" type="text/css" href="'. $file .'"/>');
+            }
+        }
+        foreach($body as $file){
+            if(strpos( $file, '.js' ) !== FALSE){
+                array_push($newBody, '<script type="text/javascript" src="'. $file .'"></script>');
+            }else if(strpos( $file, '.css' ) !== FALSE){
+                array_push($newBody, '<link rel="stylesheet" type="text/css" href="'. $file .'"/>');
+            }
+        }
+
+        return array('head' => $newHead, 'body' => $newBody);
     }
 }
--- tests/Jocolopes/Assets/JsonTest.php
+++ tests/Jocolopes/Assets/JsonTest.php
@@ -2,9 +2,39 @@
 
 namespace Jocolopes\Assets;
 
+class FakeTypes {
+    public function getTypes(){
+        return ['universal', 'mobile'];
+    }
+}
+
 class JsonTest extends \UnitTestCase {
 
-  public function testOne(){
+  public function testAssetsLoadCorrectly(){
+    $json = new Json('tests/fakeassets.json', 'tests/fakeassets');
+    $assets = $json->getAssets();
+    $this->assertEquals(count($assets['head']), 22);
+    $this->assertEquals(count($assets['body']), 11);
+    $this->assertEquals($assets['head'][0], '/example/asset10.css');
+    $this->assertEquals($assets['head'][1], '/example/test/asset2.css');
+    $this->assertEquals($assets['body'][0], '/example/test/asset8.js');
+  }
+
+  public function testAssetsMobile(){
+    $json = new Json('tests/fakeassets.json', 'tests/fakeassets', 'Jocolopes\Assets\FakeTypes');
+    $assets = $json->getAssets();
+    $this->assertEquals(end($assets['head']), '/mobile.css');
+    $this->assertEquals(end($assets['body']), '/mobile.js');
+  }
+
+  public function testAssetsWithTag(){
+    $json = new Json('tests/fakeassets.json', 'tests/fakeassets');
+    $assets = $json->getAssetsWithTag();
+    $this->assertEquals(count($assets['head']), 22);
+    $this->assertEquals(count($assets['body']), 11);
+    $this->assertEquals($assets['head'][0], '<link rel="stylesheet" type="text/css" href="/example/asset10.css"/>');
+    $this->assertEquals($assets['head'][1], '<link rel="stylesheet" type="text/css" href="/example/test/asset2.css"/>');
+    $this->assertEquals($assets['body'][0], '<script type="text/javascript" src="/example/test/asset8.js"></script>');
   }
 
 }
